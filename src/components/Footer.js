d92c34a373b905f077a7e636c034c6d4097e696c
HarryHaw1234 <133182546+HarryHaw1234@users.noreply.github.com>
2023-12-10 22:02:54 +0630

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,9 +6,9 @@ export default function Footer () {
    <footer className="h-40 flex flex-col justify-evenly items-center shadow-lg shadow-black">
         <h1 className="text-md md:text-xl underline font-title underline-offset-8">All rights served, @FoxyBrag Inc</h1>
         <div className="w-1/2 h-fit flex justify-evenly">
-            <div className="px-2 py-2 md:px-4 md:py-4 bg-white/50 shadow-lg rounded-full"><a href="https://www.google.com" target="_blank" rel="noreferrer"><FontAwesomeIcon className="text-xl"icon={faFacebook}/></a></div>
-            <div className="px-2 py-2 md:px-4 md:py-4 bg-white/50 shadow-lg rounded-full"><a href="https://www.google.com" target="_blank" rel="noreferrer"><FontAwesomeIcon className="text-xl"icon={faInstagram}/></a></div>
-            <div className="px-2 py-2 md:px-4 md:py-4 bg-white/50 shadow-lg rounded-full"><a href="https://www.google.com" target="_blank" rel="noreferrer"><FontAwesomeIcon className="text-xl"icon={faTwitter}/></a></div>
+            <div className="w-10 h-10  bg-white/50 shadow-lg rounded-full flex justify-center items-center"><a href="https://www.google.com" target="_blank" rel="noreferrer"><FontAwesomeIcon className="text-xl"icon={faFacebook}/></a></div>
+            <div className="w-10 h-10  bg-white/50 shadow-lg rounded-full flex justify-center items-center"><a href="https://www.google.com" target="_blank" rel="noreferrer"><FontAwesomeIcon className="text-xl"icon={faInstagram}/></a></div>
+            <div className="w-10 h-10  bg-white/50 shadow-lg rounded-full flex justify-center items-center"><a href="https://www.google.com" target="_blank" rel="noreferrer"><FontAwesomeIcon className="text-xl"icon={faTwitter}/></a></div>
         </div>
     </footer>
     )
