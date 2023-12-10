b3816b4a19f1df4cabf4ff4cde33b3d3d9364e1d
HarryHaw1234 <133182546+HarryHaw1234@users.noreply.github.com>
2023-12-10 21:37:46 +0630

    Update HomeSecondPage.js

--- src/pages/home/HomeSecondPage.js
+++ src/pages/home/HomeSecondPage.js
@@ -41,7 +41,7 @@ export default function HomeSecondPage() {
         className="group relative flex justify-around  py-3  bg-blue-100 odd:border-l-4  even:border-r-4 rounded-md"
       >
         <img
-          className="h-10 w-10 rounded-full ltr:ml-3 rtl:mr-3 hover:scale-125 transition-all"
+          className="h-6 w-6 md:h-10 md:w-10 rounded-full ltr:ml-3 rtl:mr-3 hover:scale-125 transition-all"
           src={field.logo}
           alt="Field Logo"
         />
@@ -49,13 +49,13 @@ export default function HomeSecondPage() {
           <p className="text-sm font-medium text-slate-900">{field.name}</p>
           <p className="text-sm text-slate-500 truncate">{field.description}</p>
         </div>
-        <span className={` absolute top-0 w-[4px] h-0 bg-sky-700  group-hover:h-full ${isOdd ? "-right-1 " : "-left-1"} transition-all rounded-e-md`}></span>
+        <span className={`absolute top-0 w-[4px] h-0 bg-sky-700  group-hover:h-full ${isOdd ? "-right-1 " : "-left-1"} transition-all rounded-e-md`}></span>
       </li>
     );
   });
   return (
     <div className="home-second-page flex flex-col items-center md:justify-center text-center min-w-full min-h-screen">
-      <h1 className="text-3xl font-bold font-title my-10">My Experience</h1>
+      <h1 className="text-xl md:text-2xl font-bold font-title my-10">My Experience</h1>
       <ul className="p-6  w-full md:w-3/4 grid grid-cols-1 gap-x-4 gap-y-4 lg:grid-cols-2 md:gap-x-6">
         {langElements}
       </ul>
