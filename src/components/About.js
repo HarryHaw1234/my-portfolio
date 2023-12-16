afffc2e691d2346fdceff98923be72951c4e9328
HarryHaw1234 <133182546+HarryHaw1234@users.noreply.github.com>
2023-12-16 22:08:43 +0630

    Button added

--- src/components/About.js
+++ src/components/About.js
@@ -8,9 +8,9 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function About() {
   return (
-    <main className="flex flex-col justify-center content-evenly flex-wrap min-w-screen min-h-screen py-20 lg:py-32">
-      <div className="lg:flex justify-around items-center w-[90%] mx-auto">
-        <div className=" mb-8 lg:w-1/3">
+    <main className="flex flex-col justify-center content-evenly flex-wrap min-w-screen min-h-screen py-20 md:py-32">
+      <div className="md:flex justify-around items-center w-[90%] mx-auto">
+        <div className=" mb-8 md:w-1/3">
           <span className="font-title text-xl ">
             <FontAwesomeIcon
               icon={faBarsStaggered}
@@ -26,12 +26,12 @@ export default function About() {
             massa egestas mollis varius; dignissim elementum. Mollis tincidunt m
           </p>
         </div>
-        <div className="lg:w-1/3">
+        <div className="md:w-1/3">
           <img src="/about.svg" alt="About SVG" />
         </div>
       </div>
-      <div className="lg:flex justify-around items-center flex-wrap w-[90%] mt-7">
-        <div className="w-[90%] lg:w-1/3 mx-auto">
+      <div className="md:flex justify-around items-center flex-wrap w-[90%] mt-7">
+        <div className="w-[90%] md:w-1/3 mx-auto md:mx-0">
           <div className="flex items-center py-7 ">
             <FontAwesomeIcon icon={faPhoneVolume} className="text-blue-950 text-xl mr-auto" />
             <span>+95 343897382</span>
@@ -45,8 +45,8 @@ export default function About() {
             <span>No.100, Pyay Road, Yangon</span>
           </div>
         </div>
-        <div className="lg:w-1/3 gap-3">
-          <p className="text-lg font-para py-3">
+        <div className="md:w-1/3 gap-3">
+          <p className="text-md font-para py-3">
             {" "}
             Write to me if you are interested!
           </p>
@@ -54,16 +54,16 @@ export default function About() {
             <input
               type="text"
               placeholder="Your name"
-              className="border-2 border-blue-950 rounded-lg w-full py-2 px-2"
+              className="border-2 border-blue-950 rounded-md w-full py-2 px-2"
             />
             <input
               type="email"
               placeholder="Your email"
-              className="border-2 border-blue-950 rounded-lg w-full py-2 px-2"
+              className="border-2 border-blue-950 rounded-md w-full py-2 px-2"
             />
             <textarea
               placeholder="Your description"
-              className="border-2 border-blue-950 rounded-lg w-full py-3 px-2"
+              className="border-2 border-blue-950 rounded-md w-full py-3 px-2"
             />
             <button className="rounded-md border-2 px-3 py-1 bg-blue-950 text-white hover:bg-white hover:text-blue-950 transition-colors">
               Submit
--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -1,17 +1,21 @@
+import { faArrowUpFromBracket } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 export default function SinglePost(props) {
   const { id } = useParams();
   const [currentProject, setCurrentProject] = useState(props.projects[id]);
+  const navigate = useNavigate();
 
   return (
     <main className="min-w-screen min-h-screen flex flex-col justify-center items-center bg-green-50 py-20">
-      <div className="w-3/4 lg:w-2/3 min-h-[55dvh] lg:flex justify-between items-center px-5 lg:py-10 bg-white rounded-md shadow-lg">
-        <a href="https://www.google.com" target="_blank" className=" h-fit ">
+      <div className="relative w-3/4 lg:w-2/3 min-h-[55dvh] lg:flex justify-between items-center px-5 lg:py-10 bg-white rounded-md shadow-lg">
+      <button className="absolute top-0 left-0 w-10 h-10 rounded-e-md text-blue-950 hover:bg-blue-950 hover:text-white" onClick={() => navigate(-1)}><FontAwesomeIcon icon={faArrowUpFromBracket}/></button>
+        <a href="https://www.google.com" target="_blank" className="h-fit ">
           <img
             src={currentProject.image}
-            className="w-[900px] h-fit px-3 hover:contrast-50 before:content-[&gt]"
+            className="w-[900px] h-fit px-3 pt-10 md:pt-0 hover:contrast-50 before:content-[&gt]"
           />
         </a>
         <div className="mt-5">
